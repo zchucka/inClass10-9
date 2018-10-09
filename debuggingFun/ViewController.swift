eca8fbfa0f1cb7d52ca926d9565f0b0581682f9a
Chucka, Zachary Tyler <zchucka@hk223-27721.local>
2018-10-09 10:40:32 -0700

    after class

--- debuggingFun/ViewController.swift
+++ debuggingFun/ViewController.swift
@@ -9,18 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: - Debugging
-    // 1. syntax/compiler
-    // cmd + B to build and if you have errors or warnings they will show up in multiple places
-    // 2. runtime
-    // 3. logic
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // MARK: - Debugging
+        // 1. syntax/compiler
+        // cmd + B to build and if you have errors or warnings they will show up in multiple places
+        // 2. runtime
+        // to set up breakpoints using a debugger
+        // buggy code
+        /*
+        let seasons = ["spring", "summer", "fall", "winter"]
+        for i in 0...seasons.count {
+            print(seasons[i])
+        }
+         */
+        // 3. logic
+        // the caveman way is the best way (print statements)
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton)
+    {
+        print("button is pressed")
+    }
+    
 }
 
